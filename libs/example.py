3d1f1c0865b9375b1b00a97dad469b94c4434acd
navita-bansal-HPE <navita.bansal-ext@hpe.com>
2025-02-10 17:12:34 +0530

    ...

--- libs/example.py
+++ libs/example.py
@@ -26,7 +26,7 @@ def list_installed_packages():
     print("Installed packages:")
     for package in sorted_packages:
         print(package)
-        print("package_name")
+        print("package_name.")
 
 
 if __name__ == "__main__":
